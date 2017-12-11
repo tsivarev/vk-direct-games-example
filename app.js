7f7b011a96e2fdb63067ab87ce67fbaf8802381c
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-12-11 17:05:03 +0300

    Create startView and gameCanvas.

---
+++ app.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('gameCanvas');
+var context = canvas.getContext('2d');
+
+// resize the canvas to fill browser window dynamically
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth / 2;
+    canvas.height = window.innerHeight / 2;
+}
+resizeCanvas();
