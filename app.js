fb79716bc20f0cd422c5ca6191d976455239c971
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-12-12 16:49:40 +0300

    Add swipe listeners.

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */
+            document.getElementById("startView").style.backgroundColor = "lightblue";
+        } else {
+            /* right swipe */
+            document.getElementById("startView").style.backgroundColor = "red";
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */
+            document.getElementById("startView").style.backgroundColor = "black";
+        } else {
+            /* down swipe */
+            document.getElementById("startView").style.backgroundColor = "white";
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
+
 var canvas = document.getElementById('gameCanvas');
 var context = canvas.getContext('2d');
 
@@ -9,6 +53,13 @@ function resizeCanvas() {
     canvas.height = window.innerHeight / 2;
 }
 resizeCanvas();
+
+// if (canvas.getContext) {
+//     var ctx = canvas.getContext('2d');
+//     // drawing code here
+// } else {
+//     // canvas-unsupported code here
+// }
 
 function startGame() {
     document.getElementById('startView').style.display = "none";
