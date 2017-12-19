e45abfff5423ceb9450e9a06ad0633ce695eb8eb
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-12-19 13:44:51 +0300

    snippet test commit

--- draw.js
+++ draw.js
@@ -123,7 +123,10 @@ var drawModule = (function () {
         // xmlHttp.send();
 
         // VK.callMethod("showLeaderboardBox", score);
-        VK.callMethod("showShareBox", "snake", "im");
+        VK.callMethod("showShareBox", score + " очков в игре Snake! Присоединяйся!",
+            [{"type": "link", "url": "https://vk.com/app6294082", "title": "Snake",
+                "photo": {"photo_604": "https://lh4.ggpht.com/yJe6HqfvYKO2ej-qfsoWf50T71o4346gXkF6T66FMXBAjJ5DhMt7buBDESUMqYaeIqI=w300"}}],
+            "wall");
     };
 
     var onCrashDisplayElements = function () {
