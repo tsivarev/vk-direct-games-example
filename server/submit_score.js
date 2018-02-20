b3606eb18bc83c824fc6472559bcd67ecb30f247
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2018-02-20 16:09:39 +0300

    Add saving user's result

---
+++ server/submit_score.js
@@ -0,0 +1,26 @@
+const AND_SIGN = '&';
+
+function sendRequest(url, params, callback) {
+    var http = new XMLHttpRequest();
+    var paramsString = params.reduce(function(result, current) {
+        return result + AND_SIGN + current;
+    }, '');
+
+    http.open('POST', url, true);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = callback(http);
+
+    http.send(paramsString);
+}
+
+function submitScore(score) {
+    var url = 'http://localhost/~Viktoria/server.php';
+    var params = ['score=' + score];
+
+    sendRequest(url, params, function(http) {
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    });
+}
