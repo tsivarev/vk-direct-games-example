303de17a26c5b1d02bf29f819dd295faae5673e4
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2018-02-20 16:39:16 +0300

    Add saving user's result

--- server/submit_score.js
+++ server/submit_score.js
@@ -1,6 +1,9 @@
 const AND_SIGN = '&';
 
+submitScore(2);
+
 function sendRequest(url, params, callback) {
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var http = new XMLHttpRequest();
     var paramsString = params.reduce(function(result, current) {
         return result + AND_SIGN + current;
@@ -17,7 +20,7 @@ function sendRequest(url, params, callback) {
 
 function submitScore(score) {
     var url = 'http://185.29.130.2/~Viktoria/server.php';
-    var params = ['score=' + score];
+    var params = ['score=' + score, 'viewer_id=100500', 'api_id=1', 'auth_key=805134f02c15fe256c4f7d196b03d9fe'];
 
     sendRequest(url, params, function(http) {
         if(http.readyState == 4 && http.status == 200) {
